d29d829e91b125b4965a8d6dc2ac0ee7d39302e2
Ramin Mamedov <mamedov10rm@mail.ru>
2023-09-05 23:18:20 +0400

    hw9

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -14,13 +14,16 @@ import {Loader} from './Loader'
 * */
 
 const HW10 = () => {
-    // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const dispatch = useDispatch();
+    const isLoading = useSelector((state: AppStoreType) => state.loading.isLoading);
+    // const isLoading = false
 
-    const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
-        // dispatch
+    const setLoading = () => {
+        dispatch(loadingAC(true));
 
-        // setTimeout
+        setTimeout(() => {
+            dispatch(loadingAC(false));
+        }, 1500);
     }
 
     return (
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,11 +1,18 @@
-const initState = {
+const initState: LoadingStateType = {
     isLoading: false,
 }
+type LoadingStateType = {
+    isLoading: boolean;
+}
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+
+export const loadingReducer = (state = initState, action: LoadingActionType): LoadingStateType => { // fix any
     switch (action.type) {
-        // пишет студент  // need to fix
-
+        case "CHANGE_LOADING":
+            return {
+                ...state,
+                isLoading: action.isLoading
+            }
         default:
             return state
     }
